87aeab66f991af3586c07eb938c558cbad124c4c
Diego Vicente <diegovic99@hotmail.com>
2022-06-23 19:29:47 -0500

    jwt

--- src/main/java/com/idat/AbrilServicioI/modelo/Items.java
+++ src/main/java/com/idat/AbrilServicioI/modelo/Items.java
@@ -2,14 +2,31 @@ package com.idat.AbrilServicioI.modelo;
 
 import java.io.Serializable;
 
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Table(name="items")
 public class Items implements Serializable{
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 6131818702692282528L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer idItem;
+	@Column
 	private String item;
+	@Column
 	private Integer cantidad;
+	@Column
 	private Double total;
 	public Integer getIdItem() {
 		return idItem;
@@ -37,6 +54,21 @@ public class Items implements Serializable{
 	}
 	public Items() {
 	}
+	
+	@JoinColumn(name = "id_cliente", 
+			nullable = false, 
+			unique = true,
+			foreignKey = @ForeignKey(foreignKeyDefinition = "foreign key (id_cliente) references clientes(id_cliente)"))
+	@ManyToOne
+	private Clientes cliente;
+	public Clientes getCliente() {
+		return cliente;
+	}
+	public void setCliente(Clientes cliente) {
+		this.cliente = cliente;
+	}
+	
+	
 	
 	
 
--- src/main/java/com/idat/AbrilServicioI/modelo/Productos.java
+++ src/main/java/com/idat/AbrilServicioI/modelo/Productos.java
@@ -1,12 +1,21 @@
 package com.idat.AbrilServicioI.modelo;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import javax.persistence.ForeignKey;
+import javax.persistence.JoinColumn;
+
 
 @Table(name = "productos")
 @Entity
@@ -59,6 +68,44 @@ public class Productos implements Serializable{
 	public void setStock(Integer stock) {
 		this.stock = stock;
 	}
+	
+	@OneToOne(mappedBy = "producto")
+	private Proveedores proveedor;
+	
+	@ManyToMany(cascade = {CascadeType.PERSIST,CascadeType.MERGE})
+	@JoinTable(
+		name="producto_cliente",
+		joinColumns =
+			@JoinColumn(
+					name="id_cliente", 
+					nullable = false, 
+					unique = true, 
+					foreignKey = @ForeignKey(foreignKeyDefinition = "foreign key(id_cliente) references clientes (id_cliente)")),
+			
+		inverseJoinColumns = 
+			@JoinColumn(
+					name="id_producto", 
+					nullable = false, 
+					unique = true, 
+					foreignKey = @ForeignKey(foreignKeyDefinition = "foreign key(id_producto) references Productos (id_producto)"))
+	)
+	private List<Clientes> cliente = new ArrayList<Clientes>();
+
+
+	public Proveedores getProveedor() {
+		return proveedor;
+	}
+	public void setProveedor(Proveedores proveedor) {
+		this.proveedor = proveedor;
+	}
+	public List<Clientes> getCliente() {
+		return cliente;
+	}
+	public void setCliente(List<Clientes> cliente) {
+		this.cliente = cliente;
+	}
+	
+	
 	
 	
 
